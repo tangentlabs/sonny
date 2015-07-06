715361fd7d74ff4c663f192350eb6f8e91e34523
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-06 09:31:22 +0100

    Decorators for easily accessing context frame

--- infrastructure/context.py
+++ infrastructure/context.py
@@ -1,3 +1,6 @@
+from functools import wraps
+
+
 class ContextContextManager(object):
     def __init__(self, context, frame):
         self.context = context
@@ -39,3 +42,23 @@ class Context(object):
 
 # Global context
 context = Context()
+
+
+def function_using_current_frame(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        frame = context.current_frame
+        injected_args = (frame,) + args
+        return func(*injected_args, **kwargs)
+
+    return decorated
+
+
+def method_using_current_frame(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        frame = context.current_frame
+        injected_args = args[:1] + (frame,) + args[1:]
+        return func(*injected_args, **kwargs)
+
+    return decorated
