074fa1f0e09245c18c00a1a5c9dec956e45fa241
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-06 14:25:41 +0100

    Rename Frame to Job, Merge Frame and FrameCM

--- infrastructure/context.py
+++ infrastructure/context.py
@@ -3,58 +3,34 @@ from functools import wraps
 import utils
 
 
-class ContextContextManager(object):
-    def __init__(self, context, frame):
-        self.context = context
-        self.frame = frame
-
-    def __enter__(self):
-        self.context._push(self.frame)
-
-        return self.frame
-
-    def __exit__(self, _type, value, traceback):
-        popped = self.context._pop()
-        assert popped == self.frame, "Popped frame was not the same as pushed"
-
-
-class Frame(object):
-    pass
-
-
 class Context(object):
     def __init__(self):
-        self.frames = []
-        self.frame_attribute_factories = {}
+        self.job_stack = []
+        self.job_attribute_factories = {}
 
-    def new_frame(self, frame=None):
-        if frame is None:
-            frame = self.create_frame()
-        return ContextContextManager(self, frame)
-
-    def create_frame(self):
-        frame = Frame()
+    def new_job(self):
+        job = Job(self)
 
         for attribute_name, attribute_factory in \
-                self.frame_attribute_factories.iteritems():
+                self.job_attribute_factories.iteritems():
             attribute = attribute_factory()
-            setattr(frame, attribute_name, attribute)
+            setattr(job, attribute_name, attribute)
 
-        return frame
+        return job
 
-    def _push(self, frame):
-        self.frames.append(frame)
+    def _push(self, job):
+        self.job_stack.append(job)
 
     def _pop(self):
-        return self.frames.pop()
+        return self.job_stack.pop()
 
     @property
-    def current_frame(self):
-        return self.frames[-1]
+    def current_job(self):
+        return self.job_stack[-1]
 
-    def auto_frame_attribute(self, attribute_name):
+    def auto_job_attribute(self, attribute_name):
         def decorator(type_or_factory):
-            self.frame_attribute_factories[attribute_name] = type_or_factory
+            self.job_attribute_factories[attribute_name] = type_or_factory
 
             return type_or_factory
 
@@ -65,54 +41,74 @@ class Context(object):
 context = Context()
 
 
-def function_using_current_frame(*frargs):
+class Job(object):
+    def __init__(self, context):
+        self.context = context
+
+    def __enter__(self):
+        self.context._push(self)
+
+        return self
+
+    def __exit__(self, _type, value, traceback):
+        popped = self.context._pop()
+        assert popped == self, "Popped job was not the same as pushed"
+
+
+def function_using_current_job(*attributes):
     def decorator(func):
         @wraps(func)
         def decorated(*args, **kwargs):
-            frame = context.current_frame
-            if frargs:
-                frame_args = tuple(getattr(frame, frarg) for frarg in frargs)
+            job = context.current_job
+            if attributes:
+                job_args = tuple(
+                    getattr(job, attribute)
+                    for attribute in attributes
+                )
             else:
-                frame_args = (frame,)
-            injected_args = frame_args + args
+                job_args = (job,)
+            injected_args = job_args + args
             return func(*injected_args, **kwargs)
 
         return decorated
 
-    if utils.is_argumentless_decorator(frargs):
-        func = frargs[0]
-        frargs = tuple()
+    if utils.is_argumentless_decorator(attributes):
+        func = attributes[0]
+        attributes = tuple()
         return decorator(func)
     else:
         return decorator
 
 
-def method_using_current_frame(*frargs):
+def method_using_current_job(*attributes):
     def decorator(func):
         @wraps(func)
         def decorated(*args, **kwargs):
-            frame = context.current_frame
-            if frargs:
-                frame_args = tuple(getattr(frame, frarg) for frarg in frargs)
+            job = context.current_job
+            if attributes:
+                job_args = tuple(
+                    getattr(job, attribute)
+                    for attribute in attributes
+                )
             else:
-                frame_args = (frame,)
-            injected_args = args[:1] + frame_args + args[1:]
+                job_args = (job,)
+            injected_args = args[:1] + job_args + args[1:]
             return func(*injected_args, **kwargs)
 
         return decorated
 
-    if utils.is_argumentless_decorator(frargs):
-        func = frargs[0]
-        frargs = tuple()
+    if utils.is_argumentless_decorator(attributes):
+        func = attributes[0]
+        attributes = tuple()
         return decorator(func)
     else:
         return decorator
 
 
-def creating_new_frame(func):
+def creating_new_job(func):
     @wraps(func)
     def decorated(*args, **kwargs):
-        with context.new_frame():
+        with context.new_job():
             return func(*args, **kwargs)
 
     return decorated
