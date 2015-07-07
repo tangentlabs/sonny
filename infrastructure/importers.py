5bbe334cd72df871c0bb0b6ff6107851a773c922
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-07 10:06:12 +0100

    Some more defaults classes for doc. purposes

--- infrastructure/importers.py
+++ infrastructure/importers.py
@@ -9,7 +9,32 @@ from infrastructure.savers import DbSaver
 from infrastructure.file_deleters import LocalFileDeleter
 
 
-class FtpCsvDbImporter(object):
+class BaseImporter(object):
+    """
+    A basic interface for importers
+    """
+
+    @classmethod
+    @context.creating_new_job
+    def create_and_run(cls, *args, **kwargs):
+        importer = cls(*args, **kwargs)
+        importer.run_import()
+
+    @context.auto_method_section
+    @utils.not_implemented
+    def run_import(self):
+        pass
+
+
+class FetchLoadInsertDeleteCleanupImporter(object):
+    """
+    As the name describes:
+    * Fetch a file
+    * Load it
+    * Insert it into a data store
+    * Delete the fetched files
+    * Run a post-job script
+    """
     ftp_registry = None
     db_registry = None
 
@@ -18,24 +43,38 @@ class FtpCsvDbImporter(object):
     insert_query = None
     post_job_query = None
 
-    @classmethod
-    @context.creating_new_job
-    def create_and_run(cls):
-        importer = cls()
-        importer.run_import()
+    fetcher = None
+    _loader = None
+    saver = None
+    deleter = None
 
     @context.auto_method_section
     def run_import(self):
         filenames = self.get_filenames()
-        local_filenames = FtpFetcher(self.ftp_registry, self.files_source).fetch_files(filenames)
+        local_filenames = self.fetcher(self.ftp_registry, self.files_source).fetch_files(filenames)
         for local_filename in local_filenames:
-            data = CsvLoader().get_all_data(local_filename)
+            data = self._loader().get_all_data(local_filename)
             data = dicts_to_tuples(self.insert_query_fields)(data)
-            DbSaver(self.db_registry, self.insert_query).save(data)
-        LocalFileDeleter().delete_files(local_filenames)
-        DbSaver(self.db_registry, self.post_job_query).save_no_data()
+            self.saver(self.db_registry, self.insert_query).save(data)
+        self.deleter().delete_files(local_filenames)
+        self.saver(self.db_registry, self.post_job_query).save_no_data()
 
-    @utils.not_implemented
     @context.auto_method_section
+    @utils.not_implemented
     def get_filenames(self):
+        """The filenames to fetch from the remote location"""
         pass
+
+
+class FtpCsvDbImporter(FetchLoadInsertDeleteCleanupImporter):
+    """
+    A more specific improter:
+    * Use FTP for importing
+    * Use CSV for loading
+    * Use a DB for inserting
+    * Use a local file deleter
+    """
+    fetcher = FtpFetcher
+    _loader = CsvLoader
+    saver = DbSaver
+    deleter = LocalFileDeleter
