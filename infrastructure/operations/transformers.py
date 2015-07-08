e6550179c566265aadb505f9891b0d84c831d67a
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-08 20:34:22 +0100

    Docstrings for transformers

--- infrastructure/operations/transformers.py
+++ infrastructure/operations/transformers.py
@@ -4,6 +4,10 @@ from infrastructure import context
 
 
 def keep_keys(keys):
+    """
+    Do a filter on columns
+    """
+
     @context.job_step
     def do_keep_keys(inputs):
         return (
@@ -19,6 +23,11 @@ def keep_keys(keys):
 
 
 def dicts_to_tuples(keys):
+    """
+    Convert dicts to tuples, using a key ordering. Potentionally doing a filter
+    on columns, if not all dict keys are in the `keys`
+    """
+
     @context.job_step
     def do_dicts_to_tuples(inputs):
         return (
@@ -34,7 +43,7 @@ def dicts_to_tuples(keys):
 
 def _maybe_cast_value(maybe_caster, value):
     """
-    Cast a value if there is a caster
+    Cast a value if there is a caster, else return the value
     """
     if not maybe_caster:
         return value
@@ -43,6 +52,10 @@ def _maybe_cast_value(maybe_caster, value):
 
 
 def cast_dicts_values(keys_casts):
+    """
+    Cast (some of) the dict's values, using a dictionary of casters
+    """
+
     @context.job_step
     def do_cast_dicts_values(inputs):
         return (
@@ -57,6 +70,10 @@ def cast_dicts_values(keys_casts):
 
 
 def generic_map(func):
+    """
+    A generic mapper using a function that operates on rows
+    """
+
     @context.job_step
     def do_generic_map(inputs):
         return itertools.imap(func, inputs)
@@ -65,6 +82,10 @@ def generic_map(func):
 
 
 def update_with_static_values(static_values):
+    """
+    Update ech dict with static values
+    """
+
     static_values_items = static_values.items()
 
     def update_with_static_values_for_input(_input):
