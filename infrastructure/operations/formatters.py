29019b6aec7617997531e22d634ce28ac85e59c3
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-09 19:15:31 +0100

    Add Bristan & Twyford job

--- infrastructure/operations/formatters.py
+++ infrastructure/operations/formatters.py
@@ -1,2 +1,20 @@
-def date_rfc_2822(_date):
-    return _date.strftime("%d %b %Y")
+def date_format(_format):
+    def do_date_format(_date):
+        return _date.strftime(_format)
+
+    return do_date_format
+
+
+def register_date_format(name):
+    """
+    Helper decorator to register date formatters
+    """
+    def decorator(func):
+        setattr(date_format, name, func)
+
+        return func
+
+    return decorator
+
+
+register_date_format("rfc_2822")(date_format("%d %b %Y"))
