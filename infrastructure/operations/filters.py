3c06b104afbe2e85ce4c1283a844b1724e4c7119
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-09 16:20:51 +0100

    Add Baxi job

---
+++ infrastructure/operations/filters.py
@@ -0,0 +1,26 @@
+from infrastructure import context
+
+
+def _input_matches_static_values(static_values, _input):
+    for key, value in static_values.iteritems():
+        if key not in _input:
+            return False
+
+        if _input[key] != value:
+            return False
+
+    return True
+
+
+def exclude_if_matching_static_values(static_values):
+    """
+    Only keep rows that don't match all key-value pairs from static_values
+    """
+
+    @context.job_step
+    def do_exclude_if_matching_static_values(inputs):
+        for _input in inputs:
+            if not _input_matches_static_values(static_values, _input):
+                yield _input
+
+    return do_exclude_if_matching_static_values
---
+++ infrastructure/operations/formatters.py
@@ -0,0 +1,2 @@
+def date_rfc_2822(_date):
+    return _date.strftime("%d %b %Y")
