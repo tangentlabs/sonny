b6b2259d64acd6804d34a5e84edd636bbd2bfe85
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-09 18:24:24 +0100

    Have GenericConfigRegistry base class

--- infrastructure/facilities/db_registry.py
+++ infrastructure/facilities/db_registry.py
@@ -1,34 +1,8 @@
-import utils
+from infrastructure.facilities.generic_config_registry import GenericConfigRegistry
 
-from infrastructure.context import \
-    function_using_current_job, method_using_current_job, context
-
-
-class BaseDbRegistry(object):
-    @utils.must_be_implemented_by_subclasses
-    def __init__(self, *args, **kwargs):
-        pass
-
-    @utils.must_be_implemented_by_subclasses
-    def __getitem__(self, key):
-        pass
+from infrastructure.context import context
 
 
 @context.register_job_facility_factory("db_registry")
-class DbRegistry(BaseDbRegistry):
-    def __init__(self, job):
-        self._db_registry = None
-
-    @method_using_current_job("config")
-    def _get_db_registry(self, config):
-        return config.db_registry
-
-    @property
-    def db_registry(self):
-        if self.db_registry is None:
-            self._db_registry = self._get_db_registry()
-
-        return self._get_db_registry
-
-    def __getitem__(self, key):
-        return self.db_registry[key]
+class DbRegistry(GenericConfigRegistry):
+    registry_config_name = "db_registry"
