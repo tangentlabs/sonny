f8ced4b899705b24b63b3f5af5d59c284dd30dcc
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-09 11:30:33 +0100

    Handy message for test/run non-existing job

--- run.py
+++ run.py
@@ -12,6 +12,10 @@ def main():
 
     package_and_class_name, args = sysargs[0], sysargs[1:]
     importer_class = locate(package_and_class_name)
+    if not importer_class:
+        raise Exception("Import '%s' could not be found. Please make sure the "
+                        "path is correct, and in python import format, and "
+                        "try again" % package_and_class_name)
     importer_class.run_from_command_line(args)
 
 if __name__ == '__main__':
