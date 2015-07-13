84e744fde54e114268beec02b19af9627a434436
Costas Basdekis <costas.basdekis@gmail.com>
2015-07-13 17:23:58 +0100

    More transformers testst

--- test/infrastructure/operations/test_transformers.py
+++ test/infrastructure/operations/test_transformers.py
@@ -6,12 +6,12 @@ from ddt import ddt, data, unpack
 from infrastructure import context
 from infrastructure.facilities import *  # noqa
 
-from infrastructure.operations.transformers import keep_keys, dicts_to_tuples
+from infrastructure.operations import transformers
 
 
 @ddt
 class TestKeepKeys(unittest.TestCase):
-    transformer = staticmethod(keep_keys)
+    transformer = staticmethod(transformers.keep_keys)
 
     @data(
         # No input
@@ -44,7 +44,7 @@ class TestKeepKeys(unittest.TestCase):
 
 @ddt
 class TestDictsToTuples(unittest.TestCase):
-    transformer = staticmethod(dicts_to_tuples)
+    transformer = staticmethod(transformers.dicts_to_tuples)
 
     @data(
         # No input
@@ -82,3 +82,125 @@ class TestDictsToTuples(unittest.TestCase):
         else:
             result = list(transformer(inputs))
             self.assertEquals(result, expected)
+
+
+@ddt
+class TestCastDictValues(unittest.TestCase):
+    transformer = staticmethod(transformers.cast_dicts_values)
+
+    doubler = lambda x: 2 * x
+
+    @data(
+        # No input
+        ({'a': doubler}, [], []),
+        # Empty keys and input
+        ({}, [{}], [{}]),
+        # Empty keys
+        ({}, [{'a': 'b'}], [{'a': 'b'}]),
+        # Empty input
+        ({'a': doubler}, [{}], [{}]),
+        # Same keys
+        ({'a': doubler, 'b': doubler}, [{'a': 1, 'b': 2}], [{'a': 2, 'b': 4}]),
+        # More keys
+        ({'a': doubler, 'b': doubler}, [{'a': 1}], [{'a': 2}]),
+        # Less keys
+        ({'a': doubler}, [{'a': 1, 'b': 2}], [{'a': 2, 'b': 2}]),
+        # Less and more keys
+        ({'a': doubler, 'c': doubler}, [{'a': 1, 'b': 2}], [{'a': 2, 'b': 2}]),
+        # Less and more keys, heterogenous
+        ({'a': doubler, 'c': doubler}, [
+            {'a': 1, 'b': 2},
+            {'b': 3, 'c': 4},
+            {'a': 5, 'c': 6},
+            {'d': 7}
+        ], [
+            {'a': 2, 'b': 2},
+            {'b': 3, 'c': 8},
+            {'a': 10, 'c': 12},
+            {'d': 7}
+        ]),
+    )
+    @unpack
+    @context.create_for_job
+    def test_transformer_output(self, casts, inputs, expected):
+        transformer = self.transformer(casts)
+
+        result = list(transformer(inputs))
+        self.assertEquals(result, expected)
+
+
+@ddt
+class TestGenericMap(unittest.TestCase):
+    transformer = staticmethod(transformers.generic_map)
+
+    nuller = lambda x: None
+
+    @data(
+        # No input
+        (nuller, [], []),
+        # Keys
+        (nuller, [{'a': 1}, {'b': 2}], [None, None]),
+        # Tuples
+        (nuller, [(1, 2), (3, 4)], [None, None]),
+    )
+    @unpack
+    @context.create_for_job
+    def test_transformer_output(self, casts, inputs, expected):
+        transformer = self.transformer(casts)
+
+        result = list(transformer(inputs))
+        self.assertEquals(result, expected)
+
+
+@ddt
+class TestUpdateWithStaticValues(unittest.TestCase):
+    transformer = staticmethod(transformers.update_with_static_values)
+
+    static_values = {'a': 1, 'b': 2}
+
+    @data(
+        # No input
+        (static_values, [], []),
+        # Less keys
+        (static_values, [{'a': 3}, {'b': 4}], [{'a': 1, 'b': 2}, {'a': 1, 'b': 2}]),
+        # Same keys
+        (static_values, [{'a': 3, 'b': 4}], [{'a': 1, 'b': 2}]),
+        # More keys
+        (static_values, [{'a': 3, 'b': 4, 'c': 5}], [{'a': 1, 'b': 2, 'c': 5}]),
+        # Less and more keys
+        (static_values, [{'a': 3, 'c': 5}], [{'a': 1, 'b': 2, 'c': 5}]),
+    )
+    @unpack
+    @context.create_for_job
+    def test_transformer_output(self, casts, inputs, expected):
+        transformer = self.transformer(casts)
+
+        result = list(transformer(inputs))
+        self.assertEquals(result, expected)
+
+
+@ddt
+class TestUpdateWithDynamicValues(unittest.TestCase):
+    transformer = staticmethod(transformers.update_with_dynamic_values)
+
+    dynamic_values = {'a': lambda x: x.get('a', 1), 'b': lambda x: 2}
+
+    @data(
+        # No input
+        (dynamic_values, [], []),
+        # Less keys
+        (dynamic_values, [{'a': 3}, {'b': 4}], [{'a': 3, 'b': 2}, {'a': 1, 'b': 2}]),
+        # Same keys
+        (dynamic_values, [{'a': 3, 'b': 4}], [{'a': 3, 'b': 2}]),
+        # More keys
+        (dynamic_values, [{'a': 3, 'b': 4, 'c': 5}], [{'a': 3, 'b': 2, 'c': 5}]),
+        # Less and more keys
+        (dynamic_values, [{'a': 3, 'c': 5}], [{'a': 3, 'b': 2, 'c': 5}]),
+    )
+    @unpack
+    @context.create_for_job
+    def test_transformer_output(self, casts, inputs, expected):
+        transformer = self.transformer(casts)
+
+        result = list(transformer(inputs))
+        self.assertEquals(result, expected)
