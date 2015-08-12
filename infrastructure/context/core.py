67db2202bb89aff237ed36bc8fbe87248e4f7e17
Costas Basdekis <costas.basdekis@gmail.com>
2015-08-12 14:46:41 +0100

    Make the framework leaner
    End up with a simpler interface

--- infrastructure/context/core.py
+++ infrastructure/context/core.py
@@ -1,223 +1,152 @@
-import utils
-
-
-"""
-Allow facilities to transparently gather metrics and meta-data, by controlling
-what global state is accessible to jobs(eg importing), and operations(eg fetch,
-load, save).
-
-A Context has a stack of Jobs.
-    A Job isolates and ties information gathered for a job run.
-A Job has a stack of JobSteps.
-    A JobStep provides the ability to structure the information hierarchically.
-"""
-
-
 class Context(object):
     """
-    An orchestrator of various facilities (logging, profiling, etc), that makes
-    the gathering of various meta-data transparent to the actual jobs, provided
-    that the code denotes the start/end of a job, and of significant job steps
+    Registry for facilities that want to operate on jobs and steps, and job
+    stack.
+    Multiple jobs can run inside each other, and each job will have it's own
+    facilities
     """
     def __init__(self):
-        # We use a Job stack to provide the facilities in a way that we don't
-        # tie global state to a specific instance. We don't expect to have
-        # more than one Job here, except in a case we want to run a Job within
-        # a Job, and want to keep the reporting sepearate to each Job.
-        self.job_stack = []
-        # Automatic attributes to add to a new Job. These factories are
-        # registered by the Context.register_job_facility_factory decorator
-        self.job_facilities_factories = {}
-        # Automatic wrappers for each job step, that want to gather information
-        # about the runtime of a step. These wrappers are registered by the
-        # Context.register_job_step_wrapper method
-        self.job_step_wrappers = []
-        # Automatic wrappers for each job  that want to gather information
-        # about the runtime of a job. These wrappers are registered by the
-        # Context.register_job_wrapper
-        self.job_wrappers = []
-        # Mixin helper classes for job classes, that provide some helper
-        # methods to the class that subclasses context.get_helpers_mixin()
-        self.importer_helper_mixins = []
-
-    def new_job(self, test):
-        """
-        Create a new Job, using the registered facilities
-        """
-        job = Job(self, test=test)
-
-        attributes = {
-            attribute_name: attribute_factory(job)
-            for attribute_name, attribute_factory
-            in self.job_facilities_factories.iteritems()
-        }
-        job._add_attributes(attributes)
-        if attributes:
-            combined_context_manager = \
-                utils.combine_context_managers(job, *attributes.itervalues())
-            return combined_context_manager
-        else:
-            return job
-
-    def _push(self, job):
-        self.job_stack.append(job)
-
-    def _pop(self):
-        return self.job_stack.pop()
+        self.facility_classes = {}
+        self._jobs = []
 
     @property
     def current_job(self):
-        return self.job_stack[-1]
-
-    def register_job_facility_factory(self, attribute_name):
-        def decorator(type_or_factory):
-            self.job_facilities_factories[attribute_name] = type_or_factory
-
-            return type_or_factory
-
-        return decorator
-
-    def register_job_wrapper(self, wrapper):
-        self.job_wrappers.append(wrapper)
-
-        return wrapper
-
-    def register_job_step_wrapper(self, wrapper):
-        self.job_step_wrappers.append(wrapper)
-
-        return wrapper
-
-    def register_importer_helper_mixin(self, cls):
-        self.importer_helper_mixins.append(cls)
-
-    def get_helpers_mixin(self):
         """
-        Create a mixin subclassing all the helper mixins registered with this
-        context.
-
-        It is better to use the get_helpers_mixin defined in this module, for
-        better portability
+        The deepest nested job, that is currently running
         """
-        super_mixin = type('ContextHelpersMixin',
-                           tuple(self.importer_helper_mixins),
-                           {})
+        return self._jobs[-1]
 
-        return super_mixin
-
-    def decorate_job(self, func):
+    def register_facility(self, name, klass):
         """
-        A decorator to wrap a job (that is a function or static method) with
-        the context's registered decorators.
-
-        Because order of imports matters in Python, you should use the
-        late-binding stand-alone decorator create_for_job, so that the step is
-        wrapped when it's called.
+        Register a facility to be created every time a job is created
         """
-        decorated = func
-        for wrapper in self.job_wrappers:
-            decorated = wrapper(decorated)
+        self.facility_classes[name] = klass
 
-        return decorated
+        return klass
 
-    def job_step(self, func):
+    def new_job(self, **kargs):
         """
-        A decorator to wrap a step (that is a function or static method) with
-        the context's registered decorators.
+        Create a Job object, that must be used as a context manager, eg:
 
-        Because order of imports matters in Python, you should use the
-        late-binding stand-alone decorator job_step, so that the step is
-        wrapped when it's called.
+        with context.new_job():
+            do_import()
         """
-        from infrastructure.context import helpers
+        job = Job(self, **kargs)
+        return job
 
-        decorated = func
-        for wrapper in self.job_step_wrappers:
-            decorated = wrapper(decorated)
+    def _push_job(self, job):
+        self._jobs.append(job)
 
-        decorated = helpers.creating_job_step(decorated)
-
-        return decorated
+    def _pop_job(self, job):
+        popped = self._jobs.pop()
+        assert popped == job
 
 
 class Job(object):
     """
-    Created by Context, it holds all the facilities tied to a specific job.
-    It should be accessed by context.current_job.
+    Facilities holder for a job run, and step stack.
 
-    It works as a context manager, and is available as a wrapper, by using
-    create_for_job.
+    Must be created with a context, and used as context manager, eg:
 
-    It keeps a stack trace of JobSteps, that the various facilities can use to
-    keep information hierarchically.
+    with context.new_job():
+        do_import()
     """
-    def __init__(self, context, test):
-        self.context = context
-        self.job_steps = []
+    def __init__(self, context, name=None, job_settings=None, test=False):
+        self.name = "<Unkown job>" if name is None else name
+        self.job_settings = job_settings
         self.test = test
-
-        self._push(JobStep(self, None, "<root>"))
-
-    def _add_attributes(self, attributes):
-        self.__dict__.update(attributes)
+        self.context = context
+        self._steps = []
+        self._create_facilities()
 
     def __enter__(self):
-        self.context._push(self)
+        self.context._push_job(self)
+        self._first_step = self.new_step(name='<root>')
+        self._first_step.__enter__()
+        self._enter_facilities()
 
         return self
 
-    def __exit__(self, _type, value, traceback):
-        popped = self.context._pop()
-        assert popped == self, "Popped job was not the same as pushed"
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._exit_facilities(exc_type, exc_value, traceback)
+        self._first_step.__exit__(exc_type, exc_value, traceback)
+        self.context._pop_job(self)
 
-    def _push(self, job_step):
-        self.job_steps.append(job_step)
+    def _create_facilities(self):
+        self.facilities = {
+            name: facility_class()
+            for name, facility_class
+            in context.facility_classes.iteritems()
+        }
+        self.facilities_list = self.facilities.values()
+        self.__dict__.update(self.facilities)
 
-    def _pop(self, expetcted):
-        popped = self.job_steps.pop()
-        assert popped == expetcted, "Popped job_step was not same as pushed"
+    def _enter_facilities(self):
+        for facility in self.facilities_list:
+            job_settings = self._get_facility_settings(facility)
+            facility.enter_job(self, job_settings)
+
+    def _get_facility_settings(self, facility):
+        default = facility.FacilitySettings
+        custom_name = '%sFacilitySettings' % facility.__class__.__name__
+        return getattr(self.job_settings, custom_name, default)
+
+    def _exit_facilities(self, exc_type, exc_value, traceback):
+        for facility in self.facilities_list[::-1]:
+            facility.exit_job(self, exc_type, exc_value, traceback)
 
     @property
-    def current_section(self):
-        return self.job_steps[-1]
+    def current_step(self):
+        return self._steps[-1]
 
-    def new_job_step(self, name):
-        """
-        A new JobStep, added in the stack when used as a context manager:
-        with job.new_job_step():
-            pass
-        """
-        return JobStep(self, self.current_section, name)
+    def new_step(self, name=None):
+        step = Step(self, name=name)
+        return step
+
+    def _push_step(self, step):
+        self._steps.append(step)
+
+    def _pop_step(self, step):
+        popped = self._steps.pop()
+        assert popped == step
 
 
-class JobStep(object):
+class Step(object):
     """
-    A job step, used by facilities to keep information hierarchically, as it
-    is denoted by the developer, that the wrapped code constitutes a single
-    step in the process.
-
-    It works as a context manager, and is available as a wrapper, by using
-    job_step.
-
-    It doesn't serve any other purpose, other than that.
+    A district and import step of a job
     """
-    def __init__(self, job, parent, name):
+    def __init__(self, job, name):
         self.job = job
-        self.parent = parent
-        if self.parent:
-            self.parents = self.parent.parents + (self.parent,)
-        else:
-            self.parents = ()
-
         self.name = name
-        if self.parent:
-            self.full_name = "%s/%s" % (self.parent.full_name, self.name)
-        else:
-            self.full_name = "%s" % self.name
-
-        self.job_steps = []
+        if self.name is None:
+            self.name = "<Job step>"
+        self.depth = None
 
     def __enter__(self):
-        self.job._push(self)
+        self.job._push_step(self)
+        self.depth = len(self.job._steps)
+        self._enter_facilities()
 
-    def __exit__(self, _type, value, traceback):
-        self.job._pop(self)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._exit_facilities(exc_type, exc_value, traceback)
+        self.job._pop_step(self)
+
+    def _enter_facilities(self):
+        for facility in self.job.facilities_list:
+            facility.enter_step(self)
+
+    def _exit_facilities(self, exc_type, exc_value, traceback):
+        for facility in self.job.facilities_list[::-1]:
+            facility.exit_step(self, exc_type, exc_value, traceback)
+
+    def wrap_step_function(self, func):
+        wrapped = func
+        for facility in self.job.facilities_list:
+            wrapped = facility.wrap_step(self, wrapped)
+
+        return wrapped
+
+
+context = Context()
--- infrastructure/context/helpers.py
+++ infrastructure/context/helpers.py
@@ -1,265 +1,68 @@
 from functools import wraps
-from abc import ABCMeta
 
-import utils
+from utils import get_callable_name
 
-from infrastructure.context import core
-
-"""
-Various helper functions that make using context easier. It is mostly
-decorators, that either inject a variable in the calling function/method, or
-trigger an action on the context itself.
-
-They are not necessary, but greatly reduce boilerplate, and standarise the
-usage of the framework.
-"""
+from infrastructure.context.core import context
 
 
-"""Global variable for context"""
-context = core.Context()
+def get_current_job():
+    return context.current_job
 
 
-def creating_job_step(func):
+def register_facility(name):
+    def decorator(klass):
+        return context.register_facility(name, klass)
+
+    return decorator
+
+
+def job(func, test=False):
     """
-    Wraps a step inside a new JobStep
-    """
-    @using_current_job
-    @wraps(func)
-    def decorated(job, *args, **kwargs):
-        with job.new_job_step(utils.get_callable_name(func)):
-            return func(*args, **kwargs)
-
-    return decorated
-
-
-def using_current_job(*attributes):
-    """
-    Decorate a method or a function automatically
-
-    @using_current_job
-    def f(job, a, b):
-        pass
-
-    @using_current_job("logger", "profiler")
-    def g(logger, profiler, a, b):
-        pass
-
-    class A(object):
-        def f(self, job, a, b):
-            pass
-
-        @classmethod
-        @using_current_job
-        def f(cls, job, a, b):
-            pass
-
-        @using_current_job("logger", "profiler")
-        def g(self, logger, profiler, a, b):
-            pass
-    """
-    def decorator(func):
-        if utils.is_method_or_class_method(func):
-            return method_using_current_job(*attributes)(func)
-        else:
-            return function_using_current_job(*attributes)(func)
-
-    if utils.is_argumentless_decorator(attributes):
-        func = attributes[0]
-        attributes = tuple()
-        return decorator(func)
-    else:
-        return decorator
-
-
-def function_using_current_job(*attributes):
-    """
-    Helper wrapper (for function and static methods), that injects the current
-    job (context.current_job), or the list of requested job attributes, if they
-    are provided:
-
-    @function_using_current_job
-    def f(job, a, b):
-        pass
-
-    @function_using_current_job("logger", "profiler")
-    def g(logger, profiler, a, b):
-        pass
-    """
-    def decorator(func):
-        @wraps(func)
-        def decorated(*args, **kwargs):
-            job = context.current_job
-            if attributes:
-                job_args = tuple(
-                    getattr(job, attribute)
-                    for attribute in attributes
-                )
-            else:
-                job_args = (job,)
-            injected_args = job_args + args
-            return func(*injected_args, **kwargs)
-
-        return decorated
-
-    if utils.is_argumentless_decorator(attributes):
-        func = attributes[0]
-        attributes = tuple()
-        return decorator(func)
-    else:
-        return decorator
-
-
-def method_using_current_job(*attributes):
-    """
-    Helper wrapper (for instance and class methods), that injects the current
-    job (context.current_job), or the list of requested job attributes, if they
-    are provided:
-
-    class A(object):
-        def f(self, job, a, b):
-            pass
-
-        @classmethod
-        @method_using_current_job
-        def f(cls, job, a, b):
-            pass
-
-        @method_using_current_job("logger", "profiler")
-        def g(self, logger, profiler, a, b):
-            pass
-    """
-    def decorator(func):
-        @wraps(func)
-        def decorated(self_or_cls, *args, **kwargs):
-            job = context.current_job
-            if attributes:
-                job_args = tuple(
-                    getattr(job, attribute)
-                    for attribute in attributes
-                )
-            else:
-                job_args = (job,)
-            injected_args = job_args + args
-            return func(self_or_cls, *injected_args, **kwargs)
-
-        return decorated
-
-    if utils.is_argumentless_decorator(attributes):
-        func = attributes[0]
-        attributes = tuple()
-        return decorator(func)
-    else:
-        return decorator
-
-
-class JobWrapper(object):
-    """
-    Helper wrapper around a function in a Job
-
-    It allows before functions, and finally functions, to be called around the
-    job run
-    """
-
-    __name__ = 'JobWrapper'
-
-    def __init__(self, func, test):
-        self.func = func
-        self.test = test
-
-    def __call__(self, *args, **kwargs):
-        return self.call(*args, **kwargs)
-
-    def call(self, *args, **kwargs):
-        decorated_with_context = self._decorate_func()
-        with context.new_job(test=self.test):
-            return decorated_with_context(*args, **kwargs)
-
-    def _decorate_func(self):
-        return context.decorate_job(self.func)
-
-
-def create_for_job(func):
-    """
-    Wrapper that runs the func inside a Job
-    """
-
-    return JobWrapper(func, test=False)
-
-
-def create_for_test_job(func):
-    """
-    Wrapper that runs the func inside a test Job
-    """
-
-    return JobWrapper(func, test=True)
-
-
-def register_current_job_importer(func):
-    """
-    Attach the importer instance to the Job
-    """
-
-    @wraps(func)
-    @using_current_job
-    def decorated(self_or_cls, job, *args, **kwargs):
-        if type(self_or_cls) in (type(object), ABCMeta):
-            cls = self_or_cls
-            importer_class = cls
-        else:
-            self = self_or_cls
-            importer_class = type(self)
-
-        if getattr(job, 'importer_class', None):
-            raise Exception("Attempting to register run two importers in same "
-                            "job: %s, while running %s" %
-                            (importer_class.__name__,
-                             job.importer_class.__name__))
-
-        job.importer_class = importer_class
-
-        return func(self_or_cls, *args, **kwargs)
-
-    return decorated
-
-
-def job_step(func):
-    """
-    The late-bindind decorator version of Context.job_step.
-
-    Use this so that the step is always wrapped using facilities that where
-    loaded after your step was declared.
+    Method/function decorator for running a job
     """
     @wraps(func)
     def decorated(*args, **kwargs):
-        decorated_with_context = context.job_step(func)
-
-        return decorated_with_context(*args, **kwargs)
+        name = get_importer_name(args)
+        job_settings = get_importer_job_settings(args)
+        with context.new_job(name=name, job_settings=job_settings, test=test) as job:
+            return func(*args, job=job, **kwargs)
 
     return decorated
 
 
-def get_helpers_mixin():
+def test_job(func):
+    return job(func, test=True)
+
+
+def step(func):
     """
-    Create a mixin subclassing all the helper mixins registered with this
-    context.
-
-    It is better to use this function, instead of context.get_helpers_mixin,
-    for better protability
+    Method/function decorator for running a step
     """
-    return context.get_helpers_mixin()
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        name = get_callable_name(func)
+        with context.current_job.new_step(name=name) as step:
+            wrapped = step.wrap_step_function(func)
+            return wrapped(*args, **kwargs)
+
+    return decorated
 
 
-def register_job_facility_factory(attribute_name):
-    return context.register_job_facility_factory(attribute_name)
+def get_importer_job_settings(args):
+    from import_jobs.base import Importer
+
+    if args and isinstance(args[0], Importer):
+        importer = args[0]
+        return importer.JobSettings
+
+    return None
 
 
-def register_importer_helper_mixin(cls):
-    return context.register_importer_helper_mixin(cls)
+def get_importer_name(args):
+    from import_jobs.base import Importer
 
+    if args and isinstance(args[0], Importer):
+        importer = args[0]
+        return importer.name
 
-def register_job_step_wrapper(wrapper):
-    return context.register_job_step_wrapper(wrapper)
-
-
-def register_job_wrapper(wrapper):
-    return context.register_job_wrapper(wrapper)
+    return None
