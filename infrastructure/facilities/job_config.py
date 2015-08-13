94d0b979df1e0707fdaf7825d471ede3bae0183a
Costas Basdekis <costas.basdekis@gmail.com>
2015-08-13 11:23:40 +0100

    Properly use cached property

--- infrastructure/facilities/job_config.py
+++ infrastructure/facilities/job_config.py
@@ -13,16 +13,27 @@ class JobConfig(Facility):
     def enter_job(self, job, facility_settings):
         super(JobConfig, self).enter_job(job, facility_settings)
 
+        self._config = None
+
+    def exit_job(self, job, exc_type, exc_value, traceback):
+        del self._config
+
     def __getitem__(self, key):
         return self.config[key]
 
     @property
     def config(self):
+        if self._config is None:
+            self._config = self._load_config()
+
+        return self._config
+
+    def _load_config(self):
         filename = self.facility_settings.filename
         config_environment = self.job.config.config_environment
-        self.config = self._get_job_environment_config(filename, config_environment)
+        config = self._get_job_environment_config(filename, config_environment)
 
-        return self.config
+        return config
 
     def _get_job_environment_config(self, job_config_filename, config_environment):
         if not job_config_filename:
