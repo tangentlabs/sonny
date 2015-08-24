1a0840f1d56609c0eab70441a01991c53ebc5211
Costas Basdekis <costas.basdekis@gmail.com>
2015-08-24 08:25:40 +0100

    Convert DB/FTP/Email registry config to flat dicts

--- infrastructure/facilities/email_registry.py
+++ infrastructure/facilities/email_registry.py
@@ -6,3 +6,9 @@ from infrastructure.context import helpers
 @helpers.register_facility("email_registry")
 class EmailRegistry(GenericConfigRegistry):
     registry_config_name = "email_registry"
+
+    def get_email_server(self, alias):
+        """
+        Get all the email server info for an email server alias
+        """
+        return self[alias]
--- infrastructure/facilities/ftp_registry.py
+++ infrastructure/facilities/ftp_registry.py
@@ -6,3 +6,9 @@ from infrastructure.context import helpers
 @helpers.register_facility("ftp_registry")
 class FtpRegistry(GenericConfigRegistry):
     registry_config_name = "ftp_registry"
+
+    def get_ftp_server(self, alias):
+        """
+        Get all the FTP server info for an FTP server alias
+        """
+        return self[alias]
