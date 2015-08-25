73444dedb94fc531852bae91c8ec75823eb3c1ba
Costas Basdekis <costas.basdekis@gmail.com>
2015-08-25 11:37:52 +0100

    Allow casters.to_date to take multiple formats

--- infrastructure/operations/casters.py
+++ infrastructure/operations/casters.py
@@ -3,9 +3,22 @@
 import time
 
 
-def to_date(date_format):
+def to_date(*date_formats):
+    date_formats_str = ', '.join(
+        "'%s'" % date_format.replace('%', '%%')
+        for date_format in date_formats
+    )
+    date_formats_value_error = \
+        "time data '%%s' does not match any of the formats %s" % date_formats_str
+
     def do_to_date(value):
-        return time.strptime(value, date_format)
+        for date_format in date_formats:
+            try:
+                return time.strptime(value, date_format)
+            except ValueError:
+                pass
+
+        raise ValueError(date_formats_value_error % value)
 
     return do_to_date
 
