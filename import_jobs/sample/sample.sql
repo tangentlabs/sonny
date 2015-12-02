46b3d91ca258b0ed9b48bbd421cbe8d07b517de0
natalia.kapralova <natalia.kapralova@tangentsnowball.com>
2015-12-02 18:02:34 +0000

    Updated the config of for the dev enviroment

--- import_jobs/sample/sample.sql
+++
@@ -1,9 +0,0 @@
-INSERT IGNORE INTO tbl_DB_AccountNew
-    (Id, AccountNo, CustomerName, Address1,
-        Address2, Address3, Address4, Postcode, Phone, OpenedDate,
-        ModifiedDate, IsDeleted)
-VALUES
-    (NULL, %(Account Number)s, %(Customer Name)s, %(Address 1)s,
-        %(Address 2)s, %(Address 3)s, %(Address 4)s,
-        %(Telephone)s, STR_TO_DATE(%(Opened)s, "%%d/%%m/%%Y"),
-        NOW(), 'N')
