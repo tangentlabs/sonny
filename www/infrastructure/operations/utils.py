ce77046a34d8b1d3a0c55c805e434ac195fc3610
Miloud Belarbi <miloud.belarbi@tangentsnowball.com>
2016-01-18 16:47:08 +0000

    Fixed variable name issue

--- www/infrastructure/operations/utils.py
+++ www/infrastructure/operations/utils.py
@@ -3,5 +3,5 @@
 def batch(iterable, batch_size=15000):
     data_list = list(iterable)
     data_size = len(data_list)
-    for slice_size in range(0, size, batch_size):
+    for slice_size in range(0, data_size, batch_size):
         yield data_list[slice_size:min(slice_size + batch_size, data_size)]
