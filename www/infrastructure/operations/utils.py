f4a28266dcca07966ffb35827302af83c3b50bfc
Miloud Belarbi <miloud.belarbi@tangentsnowball.com>
2016-01-18 16:45:18 +0000

    Removed self arg form non class function

--- www/infrastructure/operations/utils.py
+++ www/infrastructure/operations/utils.py
@@ -1,6 +1,6 @@
 
 
-def batch(self, iterable, batch_size=15000):
+def batch(iterable, batch_size=15000):
     data_list = list(iterable)
     data_size = len(data_list)
     for slice_size in range(0, size, batch_size):
