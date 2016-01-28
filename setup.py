fcc759a50914ceb916c3ca26b3e977b0d13f9e26
Costas Basdekis <costas.basdekis@gmail.com>
2016-01-28 19:12:18 +0000

    [0.2] First packaged version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tangent_importer',
-    version='0.1',
+    version='0.2',
     description='Importing framework',
     url='http://github.com/tangentlabs/tangent-importer',
     author='Costas Basdekis',
