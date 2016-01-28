4121d86053c468727d50dd4808dab826810c6bc5
Costas Basdekis <costas.basdekis@gmail.com>
2016-01-28 19:21:01 +0000

    Allow projects to import and use TestAllJobs

--- tangent_importer/tests/importers/test_all_jobs.py
+++ tangent_importer/tests/importers/test_all_jobs.py
@@ -4,13 +4,16 @@ import unittest
 
 from pydoc import locate
 
+from tangent_importer import import_jobs
 from tangent_importer.infrastructure.discover_jobs import get_importers_names
 
 
 class TestAllJobs(unittest.TestCase):
+    importer_packages = [import_jobs]
 
     def test_importers_run_without_errors(self):
-        names, failures = get_importers_names(testable_only=True)
+        names, failures = get_importers_names(packages=self.importer_packages,
+                                              testable_only=True)
 
         failed_names = []
         for name in names:
@@ -19,4 +22,5 @@ class TestAllJobs(unittest.TestCase):
                 importer_class.test()
             except Exception:
                 failed_names.append(name)
-        self.assertEquals(0, len(failed_names), "Some jobs failed to test: %s" % ', '.join(failed_names))
+        self.assertEquals(0, len(failed_names), "Some jobs failed to test: %s"
+                          % ', '.join(failed_names))
