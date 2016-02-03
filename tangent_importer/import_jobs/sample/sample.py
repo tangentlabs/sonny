83f0107b405e8db68c2f9d152e112d998063050a
natalia.kapralova <natalia.kapralova@tangentsnowball.com>
2016-02-03 14:25:34 +0000

    updated sample importer

--- tangent_importer/import_jobs/sample/sample.py
+++ tangent_importer/import_jobs/sample/sample.py
@@ -3,6 +3,7 @@ from datetime import date
 from tangent_importer import utils
 
 from tangent_importer.import_jobs import common
+from tangent_importer.infrastructure.facilities.job_config import JobConfig
 
 location = utils.make_location(__file__)
 
@@ -12,25 +13,32 @@ class SampleImporter(common.FtpCsvDbImporter):
     Sample Importer
     New customers for today
     """
+    class JobSettings(common.FtpCsvDbImporter.JobSettings):
+        class JobConfigFacilitySettings(JobConfig.FacilitySettings):
+            filename = location("sample.yaml")
+
+        test_defaults = {
+            "files_to_fetch": [location("sample.csv")],
+        }
 
     uuid = '6e26c903-a4ca-4216-93b9-872765c6c888'
 
     ftp_server = "main"
 
-    insert_query = {
-        "database": "live",
+    @property
+    def file_regex(self):
+        return r'(.*)NEW_Accounts_\.(.*)\.csv'
+
+    pre_insert_queries = []
+
+    insert_queries = {
+        "database": "dashboard",
         "file": location("sample.sql"),
     }
-    post_job_query = {
-        "database": "live",
-        "file": location("sample_post.sql"),
-    }
 
-    test_defaults = {
-        "ftp_files_to_fetch": [location("sample.csv")],
-    }
-
-    def get_ftp_files_or_search_to_fetch(self):
-        is_pattern = False
-        filenames = ['NEW_Accounts_%s.csv' % date.today().strftime('%d%m%Y')]
-        return filenames, is_pattern
+    post_insert_queries = [
+        {
+            "database": "dashboard",
+            "file": location("sample_post.sql"),
+        }
+    ]
