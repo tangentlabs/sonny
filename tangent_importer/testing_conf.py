e77aca743227deaf0585eb7bf8cbff7121602980
natalia.kapralova <natalia.kapralova@tangentsnowball.com>
2016-02-04 11:06:19 +0000

    added a db test to check that TemporaryDBFacilitySettings works as expected

--- tangent_importer/testing_conf.py
+++ tangent_importer/testing_conf.py
@@ -3,6 +3,14 @@ from tangent_importer.conf.default import * # noqa
 environment = 'testing'
 
 db_registry = {
+    "test": {
+        "host": "127.0.0.1",
+        "port": 3306,
+        "username": "root",
+        "password": "password",
+        "is_disposable": True,
+        "database": "test",
+    },
 }
 
 ftp_registry = {
