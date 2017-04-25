1fec28852de1f0af6c536cf34b983c366784e9b8
Szabolcs Gyurko <szabolcs.gyurko@tangent.co.uk>
2017-04-25 17:59:37 +0100

    Extra check for the config availability

--- sonny/infrastructure/facilities/push_notifying.py
+++ sonny/infrastructure/facilities/push_notifying.py
@@ -30,30 +30,31 @@ class PushNotifier(BasePushNotifier):
 
     @helpers.step
     def notify(self, registry, job, errors):
-        for name, push_registry in registry.registry.iteritems():
-            url = push_registry["url"] + "/rest/pushNotification"
-            system = push_registry["system"]
-            subSystem = push_registry["subSystem"]
-            title = push_registry["title"]
-            body = push_registry["body"]
-            icon = push_registry["icon"]
-            link = push_registry["link"]
-            highPriority = push_registry["highPriority"]
-            ttl = push_registry["ttl"]
+        if registry:
+            for name, push_registry in registry.registry.iteritems():
+                url = push_registry["url"] + "/rest/pushNotification"
+                system = push_registry["system"]
+                subSystem = push_registry["subSystem"]
+                title = push_registry["title"]
+                body = push_registry["body"]
+                icon = push_registry["icon"]
+                link = push_registry["link"]
+                highPriority = push_registry["highPriority"]
+                ttl = push_registry["ttl"]
 
-            data = {
-                "system": system,
-                "subSystem": subSystem,
-                "title": title,
-                "body": body % (job.name, len(errors)),
-                "icon": icon,
-                "link": link,
-                "highPriority": highPriority,
-                "ttl": ttl,
-            }
-            self.notifications.append(json.dumps(data))
+                data = {
+                    "system": system,
+                    "subSystem": subSystem,
+                    "title": title,
+                    "body": body % (job.name, len(errors)),
+                    "icon": icon,
+                    "link": link,
+                    "highPriority": highPriority,
+                    "ttl": ttl,
+                }
+                self.notifications.append(json.dumps(data))
 
-            requests.post(url, data=json.dumps(data))
+                requests.post(url, data=json.dumps(data))
 
     def __str__(self):
         return '\n'.join("data: %s" % notification for notification in self.notifications)
